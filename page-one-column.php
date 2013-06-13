d08d9d3aaa613214c08158e48853f54560750a8b
Omar Jesus Bravo <ojbravo@gmail.com>
2013-06-13 10:27:35 -0400

    Updated Facebook Tags
    Cleaned up functions.php, fixed dropdown menu, other updates.

---
+++ page-one-column.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: One Column */ get_header(); ?>
+
+
+<?php while ( have_posts() ) : the_post(); ?>
+	<?php get_template_part( 'content', 'page' ); ?>
+<?php endwhile; // end of the loop. ?>
+
+<?php get_footer(); ?>
